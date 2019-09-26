ac5139ff4560d17e309c26a7389c1f6ae02ed422
Casper <C.J.Schroder@student.tudelft.nl>
2019-09-26 19:32:54 +0200

    Update taglog to only include correct functions according to syntaxcheck

--- tag_remove_log/tagLog.py
+++ tag_remove_log/tagLog.py
@@ -1,8 +1,11 @@
 import re;
 import codecs;
+from py4j.java_gateway import JavaGateway, GatewayParameters
 
+gateway = JavaGateway(gateway_parameters=GatewayParameters(port=25335))
+syntaxChecker = gateway.entry_point
 
-filename = 'Hadoop';
+filename = 'kafkatest';
 
 f = open(filename + '.txt', "r", encoding='utf-8')
 lines = f.readlines();
@@ -11,7 +14,7 @@ f.close()
 #REGEX FAILS IF LOG STATEMENT CONTAINS ; IN STRING
 regex = re.compile('(((log)|(logger))[.]((debug)|(info)|(warn)|(fatal)|(error))[^;]*;)', flags=re.I)
 
-f = open( filename + "_filtered.txt", "w", "utf-8")
+f = open( filename + "_filtered.txt", "w", encoding="utf-8")
 
 logcount = 0;
 nologcount = 0;
@@ -31,20 +34,31 @@ for l in lines:
         funcsig = l
         index += 1
     elif (index == 2):
-        f.write(funcname)
-        f.write(funcsig)
         if (regex.search(l)):
             funcbody = l
-            f.write(re.sub('(((log)|(logger))[.]((debug)|(info)|(warn)|(fatal)|(error))[^;]*;)', '', funcbody, flags=re.I))
-            f.write('1\n')
-            logcount += 1
-            #tag with 1
+            funcbody = re.sub('(((log)|(logger))[.]((debug)|(info)|(warn)|(fatal)|(error))[^;]*;)', '', funcbody, flags=re.I)
+            if (syntaxChecker.validateMethod(funcsig + funcbody)):
+                f.write(funcname)
+                f.write(funcsig)
+                f.write(funcbody)
+                f.write('1\n')
+                logcount += 1
+                #tag with 1
+            else:
+                print("Syntax incorrect for function: " + funcsig + funcbody)
         else:
             #tag with 0
             funcbody = l
-            f.write(funcbody)
-            f.write('0\n')
-            nologcount += 1
+
+            if (syntaxChecker.validateMethod(funcsig + funcbody)):
+                f.write(funcname)
+                f.write(funcsig)
+                f.write(funcbody)
+                f.write('0\n')
+                nologcount += 1
+            else:
+                print("Syntax incorrect for UNCHANGED function: " + funcsig + funcbody)
+
         index = 0;
 
 f.close()
