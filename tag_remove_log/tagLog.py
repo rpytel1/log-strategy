7d27e5353e2dcf4a82b9cdabd29d8287c181c109
Casper <C.J.Schroder@student.tudelft.nl>
2019-10-01 12:33:25 +0200

    split datasets for code2vec and rnn

--- tag_remove_log/tagLog.py
+++ tag_remove_log/tagLog.py
@@ -14,7 +14,10 @@ f.close()
 #REGEX FAILS IF LOG STATEMENT CONTAINS ; IN STRING
 regex = re.compile('(((log)|(logger))[.]((debug)|(info)|(warn)|(fatal)|(error))[^;]*;)', flags=re.I)
 
-f = open( filename + "_filtered.txt", "w", encoding="utf-8")
+f = open( filename + "_filteredCode2Vec.txt", "w", encoding="utf-8")
+
+f2 = open( filename + "_filteredRNN.txt", "w", encoding="utf-8")
+
 
 logcount = 0;
 nologcount = 0;
@@ -45,10 +48,14 @@ for l in lines:
             funcbody = l
             funcbody = re.sub('(((log)|(logger))[.]((debug)|(info)|(warn)|(fatal)|(error))[^;]*;)', '', funcbody, flags=re.I)
             if (syntaxChecker.validateMethod(funcsig + funcbody)):
-                f.write(funcname)
+                #f.write(funcname)
+                f2.write(funcsig + funcbody)
+                f2.write('1\n')
                 funcsig = re.sub('( ([^\( ]*\())', 'f' + str(funccounter) + '_1', funcsig)
                 f.write(funcsig)
                 f.write(funcbody)
+
+                
                 logcount += 1
                 #tag with 1
             else:
@@ -59,7 +66,9 @@ for l in lines:
             funcbody = l
 
             if (syntaxChecker.validateMethod(funcsig + funcbody)):
-                f.write(funcname)
+                f2.write(funcsig + funcbody)
+                f2.write('0\n')
+                #f.write(funcname)
                 funcsig = re.sub('( ([^\( ]*\())', ' ' + filename + '_f' + str(funccounter) + '_0(', funcsig)
                 f.write(funcsig)
                 f.write(funcbody)
