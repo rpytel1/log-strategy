950c2b75f49ed69123da95f5c5157b07186c2e63
rpytel1 <rafal.pytel1@wp.pl>
2019-10-02 09:17:30 +0200

    [JackTheLoggerNet] Reading characters into pytorch

--- JackTheLoggerNet/data_loader/data_loaders.py
+++ JackTheLoggerNet/data_loader/data_loaders.py
@@ -1,3 +1,5 @@
+import string
+import torch
 from torchvision import datasets, transforms
 from base import BaseDataLoader
 from torch.utils.data.dataset import Dataset
@@ -26,29 +28,51 @@ class CodeDataLoader(BaseDataLoader):
 
 class CodeDataset(Dataset):
     CODE_DIR = "./data/"
+    all_letters = string.ascii_letters + " .,;'"
+    n_letters = len(all_letters)
 
     def __init__(self, filename):
-        d = self.readLocalFile(filename)
+        d = self.read_local_file(filename)
         self.data = []
         self.labels = []
-
+        signature = ""
+        d = open(self.CODE_DIR + "beam_filteredRNN.txt", "r", encoding="utf-8")
         for ind, elem in enumerate(d):
-            if ind % 2 == 0:
-                self.data.append(elem)
+            if ind % 3 == 0:
+                signature = elem
+            elif ind % 3 == 1:
+                self.data.append(self.line_to_tensor(signature + elem))
             else:
-                self.labels.append(elem)
+                self.labels.append(torch.tensor(int(elem)))
 
     def __getitem__(self, index):
-        return self.data[index], self.labels[index]
+        print("__getitem__")
+        print(self.data[index])
+        print(self.labels[index])
+        return self.letter_to_tensor(self.data[index]), torch.tensor(self.labels[index])
 
     def __len__(self):
         return len(self.data)
 
-
-
-    def readLocalFile(self, filename):
+    def read_local_file(self, filename):
         # TODO: repair later to accomodate multiple names from config file
         with open(self.CODE_DIR + "beam_filteredRNN.txt", 'rb') as f:
-            d = f.read()
+            d = f.readline()
         return d
 
+    def letter_to_index(self, letter):
+        return self.all_letters.find(letter)
+
+    # Just for demonstration, turn a letter into a <1 x n_letters> Tensor
+    def letter_to_tensor(self, letter):
+        tensor = torch.zeros(1, self.n_letters)
+        tensor[0][self.letter_to_index(letter)] = 1
+        return tensor
+
+    # Turn a line into a <line_length x 1 x n_letters>,
+    # or an array of one-hot letter vectors
+    def line_to_tensor(self, line):
+        tensor = torch.zeros(len(line), 1, self.n_letters)
+        for li, letter in enumerate(line):
+            tensor[li][0][self.letter_to_index(letter)] = 1
+        return tensor
--- preprocess_for_code2vec/organizeData.py
+++ preprocess_for_code2vec/organizeData.py
@@ -1,7 +1,8 @@
 import os
 
-from DataElem import DataElem
 import subprocess
+
+from preprocess_for_code2vec.DataElem import DataElem
 
 CODE2VEC_PATH = "/Users/rafalpytel/code2vec"
 f = open('../tag_remove_log/kafkatest_filtered.txt', "r", encoding='utf-8')
@@ -28,12 +29,11 @@ for id, l in enumerate(lines):
         body = l
     elif id % 4 == 3:
         label = int(l)
-        if body.strip()!="{}": # TODO: Maybe do smth else here
+        if body.strip() != "{}":  # TODO: Maybe do smth else here
             elem = DataElem(function_name, signature, body, label)
             data.append(elem)
         else:
             print(body)
-
 
 # Organize Data into split 5/2/3
 if not os.path.isdir("val"):
@@ -44,7 +44,6 @@ if not os.path.isdir("train"):
 
 if not os.path.isdir("test"):
     os.mkdir("test")
-
 
 f_train_feats = open("train/train.java", "w", encoding="utf-8")
 f_train_labels = open("train_lables.txt", "w", encoding="utf-8")
@@ -57,17 +56,17 @@ f_val_labels = open("val_lables.txt", "w", encoding="utf-8")
 
 print(len(data))
 
-for id,elem in enumerate(data):
+for id, elem in enumerate(data):
     if id % 10 < 5:
-        f_train_feats.write(elem.signature+ elem.body)
-        f_train_labels.write(str(elem.label)+"\n")
+        f_train_feats.write(elem.signature + elem.body)
+        f_train_labels.write(str(elem.label) + "\n")
 
-    elif 7 > id%10 >= 5:
-        f_test_feats.write(elem.signature +elem.body)
-        f_test_labels.write(str(elem.label)+"\n")
+    elif 7 > id % 10 >= 5:
+        f_test_feats.write(elem.signature + elem.body)
+        f_test_labels.write(str(elem.label) + "\n")
     else:
-        f_val_feats.write(elem.signature+elem.body)
-        f_val_labels.write(str(elem.body)+"\n")
+        f_val_feats.write(elem.signature + elem.body)
+        f_val_labels.write(str(elem.body) + "\n")
 
 f_train_feats.close()
 f_train_labels.close()
@@ -81,4 +80,3 @@ f_val_labels.close()
 # RUN PREPROCESS
 os.chdir(CODE2VEC_PATH)
 subprocess.call('sh preprocess_code.sh', shell=True)
-
