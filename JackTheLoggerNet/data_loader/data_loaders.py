Unknown sign fix

JackTheLoggerNet/data_loader/data_loaders.py
@@ -55,5 +55,5 @@ class CodeCharDataset(Dataset):
     def line_to_tensor(self, line):
         tensor = torch.zeros(self.max_chars, dtype=torch.long)
         for li, letter in enumerate(line):
-            tensor[li] = self.all_letters.find(letter)
+            tensor[li] = self.all_letters.find(letter) + 1
         return tensor, torch.tensor(len(line) - 1)

JackTheLoggerNet/model/model.py
@@ -16,7 +16,9 @@ class CodeRNN(nn.Module):
         self.nb_lstm_units = nb_lstm_units
         self.batch_size = batch_size
         self.vocabulary = string.printable + " .,;'"
-        self.embedding = nn.Embedding(len(self.vocabulary), embedding_size)
+
+        ## +1 bc of unkown sign
+        self.embedding = nn.Embedding(len(self.vocabulary) + 1, embedding_size)
 
         self.get_linear_input(bidirectionality)
         # design LSTM