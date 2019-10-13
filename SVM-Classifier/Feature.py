7f030b92d4a790c6a073dbab27b5733902ef82c0
Jan Gerling <jan.gerling@posteo.de>
2019-10-13 13:43:09 +0200

    svm classfier can handle large files now, refactored to enforce separation of concerns in svm classifier

---
+++ SVM-Classifier/Feature.py
@@ -0,0 +1,48 @@
+import re
+import numpy as np
+
+
+class Feature:
+    @staticmethod
+    def extract_name(data: [str]) -> str:
+        line = data[0]
+        if '[' in line or ']' in line:
+            raise ValueError("Invalid input for feature:", data)
+        return line
+
+    @staticmethod
+    def extract_codevector(data: [str]) -> [[int], [float]]:
+        tmp = ""
+        reading = False
+        for id, line in enumerate(data):
+            if '[' in line:
+                tmp = line
+                reading = True
+            elif ']' in line:
+                tmp += line
+                rawVector = re.sub(r'\[|\|\n]', '', tmp)
+                return np.fromstring(rawVector, float, sep=' ')
+            elif reading:
+                tmp += line
+
+    @staticmethod
+    def extract_label(data: [str]) -> int:
+        for id, line in enumerate(data):
+            if len(line) <= 2:
+                label = int(line)
+                if label == 0 or label == 1:
+                    return label
+
+        raise ValueError("No label found in:", data)
+
+
+    def __init__(self, rawData: [str]):
+        self.name = Feature.extract_name(rawData)
+        self.label = Feature.extract_label(rawData)
+        self.codeVector = Feature.extract_codevector(rawData)
+        if not self.valid_feature():
+            raise ValueError("Invalid input:", rawData)
+
+    def valid_feature(self) -> bool:
+        return len(self.codeVector) == 384 and (self.label == 0 or self.label == 1) and len(self.name) > 3
+
--- SVM-Classifier/SVMClassifier.py
+++ SVM-Classifier/SVMClassifier.py
@@ -1,83 +1,74 @@
-import numpy as np
 from Evaluation import JaccardIndex, Accuracy, APrecision, Precision, Recall
-from sklearn import svm
-import re
+from sklearn import svm, linear_model
+from Feature import Feature
 import time
 from Persistence import loadModel, save
-from sklearn.model_selection import train_test_split
+import numpy as np
+
 
 DATA_PATH = "..//result//codevectors//codevectors_labeled.txt"
 MODEL_SAVEPATH = "C://Users//Jan//Desktop//log-strategy//SVM-Classifier//trained_svm"
+TRAIN_SIZE = 80000
+TEST_SIZE = 10000
+STEP_SIZE = 500
 
 
-def readInput(path: str):
-    file = open(path, "r")
-    lines = file.readlines()
-    file.close()
-    print("\nExtracted", len(lines), "lines from", path, ".")
-    return lines
+def isFeatureEnd(line: str) -> bool:
+    return ']' in line
 
-def extractVectors(data) -> [[int], [float]] :
-    vectors = []
-    tmp = ""
-    reading = False
-    for id, line in enumerate(data):
-        if '[' in line:
-            tmp = line
-            reading = True
-        elif ']' in line:
-            tmp += line
-            reading = False
-            rawVector = re.sub(r'\[|\|\n]', '', tmp)
-            codeVector = np.fromstring(rawVector, float, sep=' ').tolist()
-            vectors.append(codeVector)
-        elif reading:
-            tmp += line
+def extractFeatures(file, start:int,  batch_size: int) -> ([Feature], int, bool):
+    features = []
+    lastLineNo = 0
+    featureRaw = ""
 
-    print("Found ", len(vectors), " code vectors.")
-    return vectors
+    for line in file:
+        lastLineNo += 1
+        if lastLineNo >= start:
+            featureRaw += line
 
-def extractLabels(data) -> [int]:
-    labels = []
-    for id, line in enumerate(data):
-        if len(line) <= 2:
-            labels.append(int(line))
+            if isFeatureEnd(line):
+                features.append(Feature(featureRaw.splitlines()))
+                featureRaw = ""
+            if len(features) == batch_size:
+                print("Extracted:", len(features), "features")
+                return features, lastLineNo, False
 
-    print("Of the", len(labels), "labels", labels.count(1), 'labels are positive.\n')
-    return labels
+    print("Extracted:", len(features), "features")
+    return features, lastLineNo, True
 
-def filterData(codeVectors, labels, condition):
-    filteredCodeVectors = []
-    filteredLabels = []
-    for id, label in enumerate(labels):
-        if label == condition:
-            filteredCodeVectors.append(np.fromiter(codeVectors[id], dtype=float))
-            filteredLabels.append(label)
+def extractData(features: [Feature]):
+    codeVectors, labels = [], []
+    for feature in features:
+        codeVectors.append(feature.codeVector)
+        labels.append(feature.label)
 
-    print("After filtering", len(filteredCodeVectors), "code vectors and", len(filteredLabels), "labels remain.")
-    return filteredCodeVectors, filteredLabels
+    return codeVectors, labels
 
-def extractPositive(codeVectors, labels):
-    return filterData(codeVectors, labels, 1)
+def get_model():
+    clf = linear_model.SGDClassifier()
+    return clf
 
-def extractNegative(codeVectors, labels):
-    return filterData(codeVectors, labels, 0)
+def incremental_train(data_path: str, stop: int, step: int) -> ([Feature], int):
+    feature_count, lastLineNo = 0, 0
+    eof = False
+    model = get_model()
 
-def split(codeVectors, labels, positive_size):
-    negative_size = 1 - positive_size
-    positiveCodeVectors, positiveLabel = extractPositive(codeVectors, labels)
-    negativeCodeVectors, negativeLabel = extractNegative(codeVectors, labels)
+    with open(data_path, "r") as file_in:
+        while not eof and feature_count < stop:
+            startTime = time.time()
+            features, lastLineNo, eof = extractFeatures(file_in, lastLineNo, step)
+            codeVectors, labels = extractData(features)
+            print(type(codeVectors[499]))
+            model = model.partial_fit(features, labels)
+            endTime = time.time()
+            executionTime = endTime - startTime
+            print("Extracting and training model with", TRAIN_SIZE, "features took:", round(executionTime, 2), 'seconds.')
 
-    finalRatio = negative_size / positive_size
-    finalNegativeCount = int(finalRatio * min(len(positiveCodeVectors), len(negativeCodeVectors)))
-    splitCodeVectors = positiveCodeVectors + negativeCodeVectors[:finalNegativeCount]
-    splitLabel = positiveLabel + negativeLabel[:finalNegativeCount]
-    print("Reconfigured data set has a positive/ negative label ratio of:", positive_size, "/", negative_size, "with", len(splitLabel), "elements.")
-    return splitCodeVectors, splitLabel
+    return model, lastLineNo
 
 # As other classifiers, SVC, NuSVC and LinearSVC take as input two arrays: an array X of size [n_samples, n_features] holding the training samples,
 # and an array y of class labels (strings or integers), size [n_samples]:
-def train(features, labels):
+def train_svm(features, labels):
     clf = svm.SVC(gamma='scale')
     clf.fit(features, labels)
     svm.SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
@@ -85,12 +76,6 @@ def train(features, labels):
     max_iter=-1, probability=False, random_state=None, shrinking=True,
     tol=0.001, verbose=False)
     return clf
-
-def extractData(path: str):
-    data = readInput(path)
-    codeVectors = extractVectors(data)
-    labels = extractLabels(data)
-    return codeVectors, labels
 
 def evaluate(prediction, label, description):
     print('\n-----------------------RESULTS-----------------------')
@@ -102,26 +87,22 @@ def evaluate(prediction, label, description):
     print("Average precision:", APrecision(prediction, label))
     print('-----------------------END RESULTS-----------------------\n')
 
-if __name__ == '__main__':
-    print("Reading and splitting data from:", DATA_PATH)
-    startTime = time.time()
-    full_codeVectors, full_labels = extractData(DATA_PATH)
-    train_codeVectors, test_codeVectors, train_labels, test_labels = train_test_split(full_codeVectors, full_labels, test_size = 0.2, random_state = 0)
-    endTime = time.time()
-    executionTime = endTime - startTime
-    print("Readind and splitting data took:", round(executionTime, 2), 'seconds.')
 
-    model = loadModel(MODEL_SAVEPATH + "_full" + ".joblib")
+if __name__ == '__main__':
+    model = loadModel(MODEL_SAVEPATH + "_800k" + ".joblib")
+    lastLineNo = 0
     if(model == None):
-        # reconfigured_codeVectors, reconfigured_labels = split(train_codeVectors + test_codeVectors, train_labels + test_labels, 0.1)
         print("Training new svm model.")
         startTime = time.time()
-        model = train(train_codeVectors, train_labels)
+        model, lastLineNo = incremental_train(DATA_PATH, TRAIN_SIZE, STEP_SIZE)
         endTime = time.time()
         executionTime = endTime - startTime
         print("Training model took:", round(executionTime, 2), 'seconds.')
-        save(model, MODEL_SAVEPATH + "_reconfigured" + ".joblib")
+        save(model, MODEL_SAVEPATH + "_800k" + ".joblib")
 
-    prediction = model.predict(test_codeVectors)
-    evaluate(prediction, test_labels, "test split of 20 percent.")
+    with open(DATA_PATH, "r") as file_in:
+        features, lastLineNo, eof = extractFeatures(file_in, lastLineNo, TEST_SIZE)
+        test_codeVectors, test_labels = extractData(features)
+        prediction = model.predict(test_codeVectors)
+        evaluate(prediction, test_labels, str(TEST_SIZE) + " test samples")
 
