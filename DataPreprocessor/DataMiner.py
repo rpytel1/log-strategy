008909d0a099391b2f8826d7b981af8df3102829
Jan Gerling <jan.gerling@posteo.de>
2019-10-18 15:38:34 +0200

    minor changes to DataMiner

--- DataPreprocessor/DataMiner.py
+++ DataPreprocessor/DataMiner.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
     print("Please run the TermExtractor.App class with VM arguments: -Xmx4g -Xmx8g -XX:+UseG1GC.")
 
     # 1. extract java methods from git repositories
+    print("Extracting java methods with JavaExtractor from", INPUT_PATH)
     success = java_extractor.extractMethods(INPUT_PATH, METHODS_SAVE_PATH)
     if not success:
         raise ValueError("Invalid path to repositories:", INPUT_PATH)
@@ -24,3 +25,8 @@ if __name__ == '__main__':
 
     # 3. prepare the data for code2vec, by chunking it into small files
     prepare(METHODS_SAVE_PATH + "//filteredCode2Vec", METHODS_SAVE_PATH + "//filteredCode2Vec")
+
+    #4.
+    print("To generate the codevectors with code2vec run the following code2vec.py with these arguments:\n",
+          "--load C:/Users/Jan/Desktop/log-strategy/code2vec/models/java14_model/saved_model_iter8.release\n" 
+          "--inputData C:/Users/Jan/Desktop/log-strategy/DataPreprocessor/data/filteredCode2Vec/ --representation")
