9917fc235f287346a3b9c151ae8a342b2866d046
Jan Gerling <jan.gerling@posteo.de>
2019-10-21 14:15:28 +0200

    added script with repositories for second test set

---
+++ DataPreprocessor/script/selected_repositories_test02.sh
@@ -0,0 +1,6 @@
+# Apache
+git clone https://github.com/apache/ant.git
+git clone https://github.com/apache/ant.git
+git clone https://github.com/apache/commons-lang.git
+git clone https://github.com/apache/metron.git
+git clone https://github.com/apache/pdfbox.git
