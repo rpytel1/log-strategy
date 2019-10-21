14bda3dda93373d670bfcc18efb97a41a7816780
Jan Gerling <jan.gerling@posteo.de>
2019-10-21 15:46:09 +0200

    added Preprocessor for Classifier

--- Classifier/SampleReader.py
+++ Classifier/SampleReader.py
@@ -1,5 +1,4 @@
 from Sample import Sample
-from Persistence import write
 import random
 
 # read code2vec representation from txt file into samples in an iterative more RAM efficient way
@@ -125,15 +124,4 @@ def statistics_samples(samples: [Sample]):
     negative_count = conditionalCount(samples, filterNegative)
     return (negative_count + positive_count), positive_count, negative_count
 
-def write_rebalanced_shuffled_data(file_in, save_path: str, stop: int = -1, balance: float = -1, step: int = 60000):
-    eof = False
-    sample_count: int = 0
-    while not eof and (stop < 0 or sample_count < stop):
-        samples, eof = extractSamples(file_in, min(step, max(stop - sample_count, 100) if stop > 0 else step))
-        if balance > 0:
-            samples = rebalance_data(samples, balance)
-        else:
-            samples = shuffle_data(samples)
-        sample_count += len(samples)
-        write(samples, save_path)
 
