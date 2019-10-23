c5c5477005602d5e98d54adf3a28521b17081ef3
Casper <C.J.Schroder@student.tudelft.nl>
2019-10-23 11:18:23 +0200

    balanced accuracy metric

--- JackTheLoggerNet/model/metric.py
+++ JackTheLoggerNet/model/metric.py
@@ -20,3 +20,17 @@ def recall(output, target):
         if tp_fn == 0:
             return 0
     return tp / max(tp_fn, tp)
+
+def balanced_acc(output, target):
+    rec = recall(output, target)
+
+    
+    with torch.no_grad():
+        pred = torch.argmax(output, dim=1)
+        assert pred.shape[0] == len(target)
+        pred = pred[pred == target]
+        tn = torch.sum(pred == 0).item()
+        tn_fp = torch.sum(target == 0).item()
+        if tn_fp == 0:
+            return 0
+    return (rec + (tn / max(tn_fp, tn))) / 2
